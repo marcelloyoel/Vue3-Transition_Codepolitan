df5aa509157955ae5dfaf979e7bdbb4f64274d6f
Marcello Yoel Christianus <marcelloyoell@gmail.com>
2024-09-20 12:15:07 +0700

    kenapa dia auto ketengah ya kalo resolusi 1024

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,28 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue';
+
+
+const showText = ref(false);
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Transition name="fade">
+      <h1 v-if="showText">Hello World</h1>
+    </Transition>
+    <button @click="showText = !showText">Toggle</button>
   </main>
 </template>
+
+<style scoped>
+.fade-enter-from{
+  opacity: 0;
+}
+.fade-enter-to{
+  opacity: 1;
+}
+.fade-enter-active{
+  transition: all 1s ease;
+}
+</style>
